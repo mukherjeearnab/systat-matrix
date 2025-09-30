63f10e2666909f44f93e83713d42be2aa78f0a99
Arnab Mukherjee <mukherjeearnab905@gmail.com>
2025-09-30 16:32:52 +0530

    resolved clean script

--- clean.sh
+++ clean.sh
@@ -1,9 +1,10 @@
-sudo systemctl stop systat.timer
-sudo systemctl disable systat.timer
-sudo systemctl stop systat.service
-sudo systemctl disable systat.service
+systemctl stop systat.timer
+systemctl disable systat.timer
+systemctl stop systat.service
+systemctl disable systat.service
 
-sudo rm /etc/systemd/system/systat.timer
-sudo rm /etc/systemd/system/systat.service
+rm /etc/systemd/system/systat.timer
+rm /etc/systemd/system/systat.service
 
-sudo systemctl daemon-reload
+systemctl daemon-reload
+
