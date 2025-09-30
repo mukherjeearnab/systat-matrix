5efca1b7477e703f05eddc211ddad9d7e92b501a
Arnab Mukherjee <mukherjeearnab905@gmail.com>
2025-09-30 16:38:06 +0530

    updated clean.sh

--- clean.sh
+++ clean.sh
@@ -1,7 +1,7 @@
 systemctl stop systat.timer
-systemctl disable systat.timer
+systemctl disable --now systat.timer
 systemctl stop systat.service
-systemctl disable systat.service
+systemctl disable --now systat.service
 
 rm /etc/systemd/system/systat.timer
 rm /etc/systemd/system/systat.service
